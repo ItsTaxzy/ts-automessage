c62a16aaab60bd461f31c3019b77ea32d427ad45
Taxzy <152337486+ItsTaxzy@users.noreply.github.com>
2025-01-07 14:45:46 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+author '2nicefortaxzy'
+description 'TS Automessage'
+version '1.0.0'
+
+client_scripts {
+    "client.lua",
+    "config.lua" 
+}
