f2908b3c48e3179a248aaf5bf6ee174ef94d491a
Taxzy <152337486+ItsTaxzy@users.noreply.github.com>
2025-01-07 14:45:08 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+config={}
+
+config.time = 5 			            -- Time in minutes
+config.name = 'TS AUTOMESSAGE' 	        -- Chatsender Name
+config.enabled = true 			        -- Enable or Disable Chat Automessage
+config.messages = {			            -- Chatsender Message (Make as many as you want)
+    'Welcome in [servername], have a nice time here',
+    'Thank you for using Taxzy Services Automessage.',
+}
